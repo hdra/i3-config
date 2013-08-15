b0d1321a6c1ef29767ccc2ca3e997d5bc89cec21
Hendra <hendra2392@gmail.com>
2013-08-15 16:10:26 +0700

    stuffs

--- .i3/set-wall.sh
+++ .i3/set-wall.sh
@@ -2,4 +2,5 @@
 
 #feh --bg-fill /media/ent/Wallpapers/Walls3/batman.jpg
 #feh --bg-fill /media/ent/Wallpapers/Walls4/Dx5P0.jpg
-feh --bg-fill /media/ent/Wallpapers/Walls4/1920x1080_ef60eb3e1fe504a76fbf4f433389188f201110280756041.jpeg
+#feh --bg-fill /media/ent/Wallpapers/Walls4/1920x1080_ef60eb3e1fe504a76fbf4f433389188f201110280756041.jpeg
+feh --bg-fill /media/ent/Wallpapers/Walls5/2560x1600_60dcad1eb7599b19b1e281cb2449cc91201304181206001.jpeg
